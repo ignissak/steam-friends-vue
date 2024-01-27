474b527e6c98f8f070ef274af31a72ba88eda914
Jakub Bordáš <methalicity@gmail.com>
2024-01-27 18:46:20 +0100

    feat: made avatar tooltip on right only on small screens

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -38,7 +38,7 @@ const storeLink = `https://store.steampowered.com/app/${props.game.appid}`;
       </section>
       <section class="flex gap-1">
         <template v-for="user in users" :key="user.steamid">
-          <div class="avatar tooltip first:tooltip-right" :data-tip="user.personaname">
+          <div class="avatar tooltip first:tooltip-right first:sm:tooltip-top" :data-tip="user.personaname">
             <div class="w-6 sm:w-8 md:w-6 rounded-full">
               <img :src="user.avatarfull" :alt="user.personaname" />
             </div></div
