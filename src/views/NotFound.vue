d55ad6401a193c11a3b15a69477d7c27ca312657
Jakub Bordáš <methalicity@gmail.com>
2024-02-26 14:32:35 +0100

    feat: better 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,8 +4,9 @@ import { inject } from 'vue';
 (inject('progress') as any).fail();
 </script>
 <template>
-  <main class="container">
+  <main class="container flex h-screen flex-col items-center justify-center">
     <h1>Not found</h1>
     <p class="text-neutral-400">The page you are looking for could not be found.</p>
+    <router-link to="/" class="btn btn-sm mt-4 w-full">Back to home</router-link>
   </main>
 </template>
