54a2d6f17a8d5a1ef34c4a1964a84565a31d07b8
Jakub Bordáš <methalicity@gmail.com>
2025-09-09 12:37:28 +0200

    fix: update Dockerfile to include pnpm update and correct Vite config for allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,10 +18,10 @@ export default defineConfig({
   },
   preview: {
     port: 4178,
-    allowedHosts: true,
+    host: '0.0.0.0',
   },
   server: {
     port: 4178,
-    allowedHosts: true,
+    host: '0.0.0.0',
   }
 })
